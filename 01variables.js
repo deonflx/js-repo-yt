2ab0a5407c77e5b55470adee9659d03591a9ccc3
deonflx <151120698+deonflx@users.noreply.github.com>
2024-08-24 08:46:54 +0530

    Add files via upload

---
+++ 01variables.js
@@ -0,0 +1,12 @@
+const accountid=85902249 /* const >constant cannot cahnge*/
+let accountemail="deonnjr10@gmail.com"
+var accountpasswrd="12345"//preffed not to use bcoz of scope issue
+accountcity="jaipur"
+let accountstate;
+//accountid=2 not allowed cannot change const vlue
+console.log(accountid);
+accountemail="deonnjr444@gmail.com"
+accountcity="kochi"
+console.table([accountid,accountemail,accountpasswrd,accountcity,accountstate])//used to print variables in table form (console.table([]))
+
+
---
+++ 02datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all code as newer version(automatically runs in newer version)
+//;alert("hello") are using nodejs not browser
+let name="deon"
+let age=18
+let isloggedin=true
+//number =2^53 aprox range
+//bigint if number is very big use big int
+//string ""
+//boolean true || false
+//null => standalone value (representation of empty space)
+//undefined => when value is not assigned
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof null); //type of null=>object
+console.log(typeof undefined);//type of undefined is undefined
